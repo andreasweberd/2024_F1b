131fb31929469ef1b7a4f369c992fe7ee2c7425d
mattamann <matthiasjramann519@gmail.com>
2024-10-11 11:07:10 +0200

    .

--- amann_matthias/soundsensor1/soundsensor1.ino
+++ amann_matthias/soundsensor1/soundsensor1.ino
@@ -1,4 +1,7 @@
 const int ledPin = 4;
+const int ledPin2 = 5;
+const int ledPin3 = 2;
+const int ledPin4 = 3;
 const int soundSensorPin = 8;
 const int soundSensorAnalogPin = A1;
 int soundThreshold = 200;
@@ -6,6 +9,9 @@ int soundThreshold = 200;
 void setup() {
   pinMode(soundSensorAnalogPin, INPUT);
   pinMode(ledPin, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
   pinMode(soundSensorPin, INPUT);
   Serial.begin (9600);
 }
@@ -15,16 +21,28 @@ void loop() {
   int soundDetectedAnalog = analogRead(soundSensorAnalogPin);
   Serial.println (soundDetectedAnalog);
 
-  if (soundDetected == HIGH) {
+  if (soundDetectedAnalog > 80) {
     digitalWrite(ledPin, HIGH);
   } else {
     digitalWrite(ledPin, LOW);
   }
 
-  if (soundDetectedAnalog > 130) {
-    digitalWrite(ledPin, HIGH);
+  if (soundDetectedAnalog > 100) {
+    digitalWrite(ledPin2, HIGH);
   } else {
-    digitalWrite(ledPin, LOW);
+    digitalWrite(ledPin2, LOW);
+  }
+
+  if (soundDetectedAnalog > 120) {
+    digitalWrite(ledPin3, HIGH);
+  } else {
+    digitalWrite(ledPin3, LOW);
+  }
+  
+  if (soundDetectedAnalog > 140) {
+    digitalWrite(ledPin4, HIGH);
+  } else {
+    digitalWrite(ledPin4, LOW);
   }
 
   delay (80);
